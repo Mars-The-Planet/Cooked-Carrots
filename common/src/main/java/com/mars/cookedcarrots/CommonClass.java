25a9dc95c4cf11f77f085b9a55bf17a9fdf844cd
Matej <ruzicka.maty@gmail.com>
2025-05-20 23:24:18 +0200

    backported to 1.20.4

---
+++ common/src/main/java/com/mars/cookedcarrots/CommonClass.java
@@ -0,0 +1,22 @@
+package com.mars.cookedcarrots;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.mars.deimos.datagen.DeimosRecipeGenerator;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+
+import java.util.HashMap;
+
+public class CommonClass {
+    public static final HashMap<String, Supplier<Item>> ITEMS = new HashMap<>();
+    public static final Supplier<Item> COOKED_CARROT = Suppliers.memoize(() -> new Item(new Item.Properties().food(
+            new FoodProperties.Builder().fast().alwaysEat().nutrition(3).saturationMod(0.6F).build())));
+    public static void init() {
+        ITEMS.put("cooked_carrot", COOKED_CARROT);
+
+        DeimosRecipeGenerator.createSmeltingJson("minecraft:carrot", "cookedcarrots:cooked_carrot", 200, 0.35f);
+        DeimosRecipeGenerator.createSmokingJson("minecraft:carrot", "cookedcarrots:cooked_carrot", 100, 0.35f);
+        DeimosRecipeGenerator.createCampfireCookingJson("minecraft:carrot", "cookedcarrots:cooked_carrot", 600, 0);
+    }
+}
---
+++ fabric/src/main/java/com/mars/cookedcarrots/CookedCarrots.java
@@ -0,0 +1,20 @@
+package com.mars.cookedcarrots;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
+
+import static com.mars.cookedcarrots.Constants.MOD_ID;
+
+public class CookedCarrots implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+        CommonClass.ITEMS.forEach((string, itemSupplier) -> Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(MOD_ID, string), itemSupplier.get()));
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                .register((itemGroup) -> CommonClass.ITEMS.forEach((string, itemSupplier) -> itemGroup.accept(itemSupplier.get())));
+    }
+}
