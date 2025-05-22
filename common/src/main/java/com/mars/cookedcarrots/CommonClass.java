aa2564072d26472eca007638f4c438f2f3087ddd
Matej <ruzicka.maty@gmail.com>
2025-05-22 17:37:10 +0200

    updated to 1.21.3

--- common/src/main/java/com/mars/cookedcarrots/CommonClass.java
+++ common/src/main/java/com/mars/cookedcarrots/CommonClass.java
@@ -5,16 +5,22 @@ import com.google.common.base.Suppliers;
 import com.mars.cookedcarrots.platform.Services;
 import com.mars.deimos.datagen.DeimosRecipeGenerator;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.Consumables;
 
 import java.util.HashMap;
 
+import static com.mars.cookedcarrots.Constants.MOD_ID;
+
 public class CommonClass {
     public static final HashMap<String, Supplier<Item>> ITEMS = new HashMap<>();
-    public static final Supplier<Item> COOKED_CARROT = Suppliers.memoize(() -> new Item(new Item.Properties().food(
-            new FoodProperties.Builder().fast().alwaysEdible().nutrition(3).saturationModifier(0.6F).build())));
+    public static final Supplier<Item> COOKED_CARROT = Suppliers.memoize(() -> new Item(new Item.Properties().useItemDescriptionPrefix().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "cooked_carrot")))
+            .food(new FoodProperties.Builder().alwaysEdible().nutrition(3).saturationModifier(0.6F).build(), Consumables.DRIED_KELP)));
 
     public static void init() {
         ITEMS.put("cooked_carrot", COOKED_CARROT);
