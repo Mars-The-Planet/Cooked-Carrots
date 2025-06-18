e56377767117184f472ebcbc1c509b50b46e671d
Mars <ruzicka.maty@gmail.com>
2025-06-18 22:29:07 +0200

    fixed forge

--- forge/src/main/java/com/mars/cookedcarrots/CookedCarrots.java
+++ forge/src/main/java/com/mars/cookedcarrots/CookedCarrots.java
@@ -3,9 +3,9 @@ package com.mars.cookedcarrots;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,11 +17,11 @@ public class CookedCarrots {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
     public CookedCarrots() {
         CommonClass.init();
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        var eventBus = FMLJavaModLoadingContext.get().getModBusGroup();
 
         CommonClass.ITEMS.forEach(ITEMS::register);
         ITEMS.register(eventBus);
-        eventBus.addListener(CookedCarrots::buildContents);
+        BuildCreativeModeTabContentsEvent.getBus(eventBus).addListener(CookedCarrots::buildContents);
     }
 
     @SubscribeEvent
